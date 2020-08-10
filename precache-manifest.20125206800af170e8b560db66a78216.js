3ff5954959a0d9c1bacf9d11ecac5f8c9cfef58a
Sidney <Hyosang@Macintoshui-MacBook-Pro.local>
2020-08-10 23:19:12 +0900

    Updates

---
+++ precache-manifest.20125206800af170e8b560db66a78216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e023a13ac722ac9d745761ea0fb984eb",
+    "url": "/sidflix/index.html"
+  },
+  {
+    "revision": "7970e95422df3e7e0b3d",
+    "url": "/sidflix/static/js/2.7d111252.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/sidflix/static/js/2.7d111252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddd7ebdc66c615a5ac2",
+    "url": "/sidflix/static/js/main.b673773d.chunk.js"
+  },
+  {
+    "revision": "18c3e2331f605ae14060",
+    "url": "/sidflix/static/js/runtime-main.7faaa7df.js"
+  },
+  {
+    "revision": "cd4d541ac83db8fa18fb526a074cc0c0",
+    "url": "/sidflix/static/media/freak.cd4d541a.jpg"
+  }
+]);
